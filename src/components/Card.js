58a7b4cee60bb2e9526233537c119eeb6c30879d
Ryan Weidling <ryanweidling@gmail.com>
2024-08-12 16:51:42 -0400

    card styling, dogData changes, style changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,24 +3,23 @@ import React from "react"
 export default function Card(props) {
     console.log(props)
 
-    let badgeText
-    if(props.dog.adopted === true) {
-        badgeText = "Adopted!"
-    } else if (props.dog.daysInShelter < 30) {
-        badgeText = "New Arrival!"
-    } else {
-        badgeText = "Available!"
-    }
+    const badgeText = props.dog.adopted === true ? "Adopted!" : props.dog.daysInShelter < 30 ? "New Arrival!" : "Available"
+
+    const badgeStyle = props.dog.adopted === true ?
+        { backgroundColor: "#FFADAD" } :
+        props.dog.daysInShelter < 30 ?
+            { backgroundColor: "#BDB2FF" } :
+            { backgroundColor: "#CAFFBF" };
 
     
     return (
         <div className="card">
-            {badgeText && <div className="card--badge" >{badgeText}</div>}
+            {badgeText && <div className="card--badge" style={badgeStyle}>{badgeText}</div>}
             <img className="card--image" src={props.dog.cardImage} alt="Golden Retriever"/>
-            <div style={{margin:"0 10px 0px 10px"}}>
+            <div className="card--info">
                 <h3 className="card--title">{props.dog.name}</h3>
                 <h4>Age: {props.dog.age}</h4>
-                {!props.dog.adopted && <h5>At shelter for {props.dog.daysInShelter} days</h5>}
+                {props.dog.adopted === false ? <h5>At shelter for {props.dog.daysInShelter} days</h5> : <h5>Happily adopted!</h5>}
             </div>
         </div>
     )
--- src/dogData.js
+++ src/dogData.js
@@ -9,64 +9,64 @@ export default [
     },
     {
         id: "2",
-        cardImage: "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcHUyMzMxNzg4LWltYWdlLXJtNTAzLTAxXzEtbDBqOXFyYzMucG5n.png",
-        name: "Golden Boy",
+        cardImage: "https://i.pinimg.com/originals/ca/4c/6b/ca4c6be8d049a58b46f9a52b2d69d764.png",
+        name: "Dog dog",
         age: "3",
         adopted: false,
         daysInShelter: "90"
     },
     {
         id: "3",
-        cardImage: "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcHUyMzMxNzg4LWltYWdlLXJtNTAzLTAxXzEtbDBqOXFyYzMucG5n.png",
-        name: "Golden Boy",
+        cardImage: "https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg",
+        name: "Bruce",
         age: "3",
         adopted: false,
         daysInShelter: "29"
     },
     {
         id: "4",
-        cardImage: "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcHUyMzMxNzg4LWltYWdlLXJtNTAzLTAxXzEtbDBqOXFyYzMucG5n.png",
-        name: "Golden Boy",
+        cardImage: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
+        name: "Little King",
         age: "3",
         adopted: false,
         daysInShelter: "43"
     },
     {
         id: "5",
-        cardImage: "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcHUyMzMxNzg4LWltYWdlLXJtNTAzLTAxXzEtbDBqOXFyYzMucG5n.png",
-        name: "Golden Boy",
+        cardImage: "https://www.hartz.com/wp-content/uploads/2022/04/small-dog-owners-1.jpg",
+        name: "Jerry",
         age: "3",
         adopted: false,
         daysInShelter: "24"
     },
     {
         id: "6",
-        cardImage: "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcHUyMzMxNzg4LWltYWdlLXJtNTAzLTAxXzEtbDBqOXFyYzMucG5n.png",
-        name: "Golden Boy",
+        cardImage: "https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/45ed8ecf-0bb2-4e34-8fcf-624db47c43c8/Golden+Retrievers+dans+pet+care.jpeg",
+        name: "Martha",
         age: "3",
         adopted: false,
         daysInShelter: "99"
     },
     {
         id: "7",
-        cardImage: "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcHUyMzMxNzg4LWltYWdlLXJtNTAzLTAxXzEtbDBqOXFyYzMucG5n.png",
-        name: "Golden Boy",
+        cardImage: "https://static.vecteezy.com/system/resources/thumbnails/008/951/892/small_2x/cute-puppy-pomeranian-mixed-breed-pekingese-dog-run-on-the-grass-with-happiness-photo.jpg",
+        name: "Squeak",
         age: "3",
         adopted: false,
         daysInShelter: "345"
     },
     {
         id: "8",
-        cardImage: "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcHUyMzMxNzg4LWltYWdlLXJtNTAzLTAxXzEtbDBqOXFyYzMucG5n.png",
-        name: "Golden Boy",
+        cardImage: "https://i0.wp.com/www.angierose.com/wp-content/uploads/2022/05/Odesa-9-years-old-1-819x1024.jpg",
+        name: "Champ",
         age: "3",
         adopted: false,
         daysInShelter: "291"
     },
     {
         id: "9",
-        cardImage: "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcHUyMzMxNzg4LWltYWdlLXJtNTAzLTAxXzEtbDBqOXFyYzMucG5n.png",
-        name: "Golden Boy",
+        cardImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEXGnj2fcov0xGprnnC688YMyxVtVPS8CAg&s",
+        name: "Cloud",
         age: "3",
         adopted: false,
         daysInShelter: "2"
