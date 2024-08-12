e05b4b5e2f610dbc47f2988c5c18933768f09635
Ryan Weidling <ryanweidling@gmail.com>
2024-08-12 10:18:52 -0400

    travel journal created

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Card() {
+    return (
+        <div className="card">
+            <h3 className="card--title">Mr. Whiskerson</h3>
+            <div className="info-group">
+                <p>(212) 555-1234</p>
+            </div>
+            <div className="info-group">
+                <p>mr.whiskaz@catnap.meow</p>
+            </div>
+        </div>
+    )
+}
